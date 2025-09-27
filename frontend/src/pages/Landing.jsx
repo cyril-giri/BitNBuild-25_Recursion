505b00501153a86a39f4d38ff7453e6924b7503c
nicolemenezes <nicmenezes35@gmail.com>
2025-09-27 22:29:07 +0530

    ProductPreviews & How It Works

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import HeroSection from "../components/Landing/HeroSection";
 import Navbar from "../components/Landing/navbar";
 import WhoAmI from "../components/Landing/WhoAmI";
+import FeatureGrid from "../components/Landing/FeatureGrid";
+import ProductPreviews from "../components/Landing/ProductPreviews";
+import HowItWorks from "../components/Landing/HowItWorks";
+
 function Landing() {
   return (
     <div className="theme-gigcampus bg-black min-h-screen text-white">
         <Navbar />
       <HeroSection />
       <WhoAmI />
+        <FeatureGrid />
+        <ProductPreviews />
+        <HowItWorks/>
     </div>
   );
 }
